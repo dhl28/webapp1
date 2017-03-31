34ac503ba88c39f1334ba2710e1b6de24ede9d18
douhl <douhl@yonyou.com>
2017-03-31 17:07:03 +0800

    add ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('webapp1App', [
     'ngAnimate',
     'ngCookies',
@@ -19,3 +19,36 @@ angular
     'ui.router',
     'ui.bootstrap'
   ]);
+
+app.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+  $locationProvider.html5Mode(true)
+  var appState = {
+    name: 'app',
+    url: '/',
+    // abstract:true,
+    template: '<ui-view></ui-view>'
+  }
+  var homeState = {
+    name: 'app.home',
+    url: '^/home',
+    template: '<h3>home page</h3>'
+  }
+  var helloState = {
+    name: 'app.hello',
+    url: '^/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'app.about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  $stateProvider.state(appState);
+  $stateProvider.state(homeState);
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+  $urlRouterProvider.otherwise('/home');
+});
+
